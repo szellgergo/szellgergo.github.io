256c73763dcebbea21a2c288e9f2977e9cca2dea
Széll Gergő <szell.gergo@innomedio.hu>
2023-11-30 20:03:58 +0100

    aktuális ár kiíratása

--- criptoGergo.js
+++ criptoGergo.js
@@ -1,15 +1,23 @@
 /**
  * Első függvény lekéri a BTC árfolyamát
+ * token  = BTCUSDT
  */
-function getTokenData(token = BTCUSDT)
-{
+function gettokendata() {
     $.ajax({
         type: 'GET',
-        url: 'https://www.binance.com/api/v3/ticker',
-        data: 'symbol=' + token,
+        url: 'https://api.binance.com/api/v3/ticker/price',
+        data: { symbol: 'BTCUSDT' },
         success: function(ex) {
-            var r1 = ex['lastPrice']*1;
-            console.log(r1);
-          }
-        });
+            var r1 = parseFloat(ex['price']);
+            console.log(ex);
+            $('#szamInput').val(r1);
+
+
+            $('#btc').text($('#btc').text() + " <br/> " + r1);
+        }
+    });
+}
+
+function getbtcprice() {
+    setInterval(gettokendata, 1000);
 }
